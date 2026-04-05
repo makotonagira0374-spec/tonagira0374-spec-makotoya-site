b977d53110be5d7e53b9da7d0c5b6953c14961e1
nagira makoto <makoto.nagira0374@gmail.com>
2026-04-05 22:40:07 +0900

    Add SEO blog section and first article

---
+++ blog/script.js
@@ -0,0 +1,9 @@
+﻿const header = document.querySelector('.blog-header');
+
+function syncHeaderState() {
+  if (!header) return;
+  header.classList.toggle('is-scrolled', window.scrollY > 8);
+}
+
+syncHeaderState();
+window.addEventListener('scroll', syncHeaderState, { passive: true });
